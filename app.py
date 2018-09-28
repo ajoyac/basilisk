7a5672200fbcf5d391d78b9f87344bfe1be04b36
Alejandro Joya <joya.a.cruz@gmail.com>
2018-09-28 03:29:04 -0500

    add login endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from resources.user import Register
+
 from flask import Blueprint
 from flask_restful import Api
+from resources.user import Register,Login
 
 
 api_bp = Blueprint('api', __name__)
@@ -8,3 +9,4 @@ api = Api(api_bp)
 
 # Route
 api.add_resource(Register, '/signup')
+api.add_resource(Login, '/login')
--- resources/user.py
+++ resources/user.py
@@ -1,8 +1,30 @@
 from flask import request
 from flask_restful import Resource
 from Model import db, User,UserSchema
+import jwt
 
 user_schema = UserSchema()
+
+class Login(Resource):
+    def post(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+               return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = user_schema.load(json_data)
+        if errors:
+            return errors, 400
+        user = User.query.filter_by(username=data['username'],password=data['password']).first()
+        if user:
+            result = user_schema.dump(user).data
+            del result["password"]
+            result ["token"] = str(jwt.encode(result, 'secret', algorithm='HS256'))
+            return { "status": 'success', 'data': result }, 201
+        else:
+             return {'message': 'No user/password found'}, 400   
+
+
+
 
 class Register(Resource):
     def post(self):
@@ -12,7 +34,7 @@ class Register(Resource):
         # Validate and deserialize input
         data, errors = user_schema.load(json_data)
         if errors:
-            return errors, 422
+            return errors, 400
         user = User.query.filter_by(username=data['username']).first()
         if user:
             return {'message': 'user already exists'}, 400
@@ -25,5 +47,8 @@ class Register(Resource):
         db.session.commit()
 
         result = user_schema.dump(user).data
+        del result["password"]
+        result ["token"] = str(jwt.encode(result, 'secret', algorithm='HS256'))
+      
 
         return { "status": 'success', 'data': result }, 201
